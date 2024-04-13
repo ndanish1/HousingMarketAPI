7c3937807f499f121c22f8f827a1756a880a19ec
Naveed Danish <ndanish1@my.centennialcollege.ca>
2024-04-13 17:30:07 -0400

    Fix bug in User and Property in Context.cs file to make Property and User as nullable

--- HousingMarket_API/HousingMarketAPIDbContext.cs
+++ HousingMarket_API/HousingMarketAPIDbContext.cs
@@ -6,9 +6,9 @@ namespace HousingMarket_API
 {
     public class HousingMarketAPIDbContext : DbContext
     {
-        public DbSet<PropertyModel> Property { get; set; } = null;
+        public DbSet<PropertyModel>? Property { get; set; } = null;
 
-        public DbSet<UserModel> User { get; set; } = null;
+        public DbSet<UserModel>? User { get; set; } = null;
         public HousingMarketAPIDbContext(DbContextOptions<HousingMarketAPIDbContext> options) : base(options)
         {
         }
